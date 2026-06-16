16ae1e0fb7d37418a0d6362637f5cfedfa5c5077
lovegitguy <filmyfunda100@gmail.com>
2026-06-16 16:29:32 +0530

    Initialize real-time currency exchanger project

---
+++ RealTimeCurrencyExchanger/Currency.java
@@ -0,0 +1,31 @@
+package RealTimeCurrencyExchanger;
+
+public class Currency {
+    private String code;
+    private String name;
+    private String symbol;
+
+    public Currency(String code, String name, String symbol)
+    {
+        this.code= code;
+        this.name= name;
+        this.symbol= symbol;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getsymbol(){
+        return symbol;
+    }
+
+    @Override
+    public String toString(){
+         return code + " - " + name + " (" + symbol + ")";
+    }
+}
